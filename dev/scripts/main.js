f2ade83a9424043d5b04ea4eb326c71f930eead7
Saad Rahman <sur207@nyu.edu>
2017-06-02 15:57:21 -0400

    layout changes

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -93,6 +93,7 @@ lcboApp.getAlc = function(userChoiceBooze) {
         let testResults = res.result;
         // console.log(testResults)
         lcboApp.displayAlc(testResults);
+        console.log(testResults);
     })
     };
 
@@ -136,8 +137,9 @@ lcboApp.displayAlc = function(item){
             name: 'options',
             value: someObj.id
         })
+        var price = $('<p>').text('$' + (someObj.price_in_cents/100))
         var label = $('<label>').attr('for', someObj.id).append(alcName,alcImg);
-        var alcContainer = $('<div>').addClass('alcContainer').append(input, label)
+        var alcContainer = $('<div>').addClass('alcContainer').append(input, label, price)
         //adding data identifier to the container so that the program identifies what we selected
         //.data('alcid', someObj.id);
         $('.masterContainer').addClass('clearfix').append(alcContainer);
@@ -220,6 +222,7 @@ lcboApp.events = function() {
         lcboApp.getStoresById();
         lcboApp.events();
     }
+
 
 
 lcboApp.init = function(){
